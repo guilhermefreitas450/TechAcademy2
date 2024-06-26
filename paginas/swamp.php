5496786c7a11ba0ee38b54262b865f3670a42d26
guilhermefreitas450 <161781523+guilhermefreitas450@users.noreply.github.com>
2024-06-25 23:14:48 -0300

    Add files via upload

---
+++ paginas/swamp.php
@@ -0,0 +1,43 @@
+<section id="swamp">
+<h1 class="text-center">Swamp Challenge</h1>
+<div class="flex2">
+<div class="container-games-mobile">
+<img src="imagens/swamp1.jfif" title="SwampChallenge" alt="SwampChallenge">
+
+<p>Em SwampChallenge, você embarca em uma jornada mágica ao lado de Luna, uma jovem e corajosa heroína em busca de tesouros e aventuras em um mundo cheio de mistérios.
+Ajude Luna a coletar todas as moedas mágicas e derrotar os monstros para restaurar a paz e harmonia na terra encantada.
+</p>
+<div class="texto-mobile">
+<p>Desenvolvedor:<a href="membros">Marcos</a></p>
+<p>Engine:<a href="https://www.construct.net/en">Contruct3</a></p>
+</div>
+<div class="btn-jogar-mobile">
+
+<a href="404" title="jogar agora">
+<button type="button" class="btn btn-danger">Incompatível</button>
+</a>
+</div>
+</div>
+<div class="flex2">
+    <div class="flex-coluna1">
+<div class="container-games-pc">
+<img src="imagens/swamp1.jfif" title="SwampChallenge" alt="SwampChallenge">
+</div>
+</div>
+<div class="flex-coluna">
+<div class="container-games-pc">
+<p> Em SwampChallenge, você embarca em uma jornada mágica ao lado de Luna, uma jovem e corajosa heroína em busca de tesouros e aventuras em um mundo cheio de mistérios.
+Ajude Luna a coletar todas as moedas mágicas e derrotar os monstros para restaurar a paz e harmonia na terra encantada.
+</p>
+<p>Desenvolvedor:<a href="membros">Marcos</a></p>
+<p>Engine:<a href="https://www.construct.net/en">Contruct3</a></p>
+<div class="btn-jogar">
+
+<a href="jogos/swampGM/index.html" target="_blank" title="jogar agora">
+<button type="button" class="btn btn-warning">Jogue Agora</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</section>
